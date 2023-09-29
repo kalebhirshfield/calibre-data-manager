341ceb6958f56b5595ba8082955fe0e289204104
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2023-09-29 23:29:42 +0100

    combined browse and search tabs

--- main.py
+++ main.py
@@ -52,14 +52,14 @@ def main(page: ft.Page):
         if e.pixels >= e.max_scroll_extent - 300:
             if sem.acquire(blocking=False):
                 try:
-                    if tabs.selected_index == 0:
+                    if tableBrowseSelection.value == "Stock Levels":
                         addDataToTable(
                             stockLevelsTable,
                             fetchStockLevels,
                             10,
                             stockLevelsTable.rows,
                         )
-                    elif tabs.selected_index == 1:
+                    elif tableBrowseSelection.value == "Historical Sales":
                         addDataToTable(
                             historicalSalesTable,
                             fetchHistoricalSales,
@@ -69,25 +69,48 @@ def main(page: ft.Page):
                 finally:
                     sem.release()
 
+    def tableSwitcher(e):
+        stockLevelsTable.visible = (
+            True if tableBrowseSelection.value == "Stock Levels" else False
+        )
+        historicalSalesTable.visible = (
+            True if tableBrowseSelection.value == "Historical Sales" else False
+        )
+        page.update()
+
     def search(e):
-        if tabs.selected_index == 2 or tabs.selected_index == 3:
+        if tabs.selected_index == 1:
             searchBar.visible = True
+            tableSearchSelection.visible = True
         else:
             searchBar.visible = False
+            tableSearchSelection.visible = False
+        if tabs.selected_index == 0:
+            tableBrowseSelection.visible = True
+        else:
+            tableBrowseSelection.visible = False
         query = str(searchBar.value.strip())
         if query != "":
-            searchHistoricalSalesTable.visible = True
-            searchStockLevelsTable.visible = True
+            searchHistoricalSalesTable.visible = (
+                True if tableSearchSelection.value == "Historical Sales" else False
+            )
+            searchStockLevelsTable.visible = (
+                True if tableSearchSelection.value == "Stock Levels" else False
+            )
             columnsToSearch = (
                 [column for column in stockLevelsColumns]
-                if tabs.selected_index == 2
+                if tableSearchSelection.value == "Stock Levels"
                 else [column for column in historicalSalesColumns]
             )
             conditions = [
                 f"CAST({column} as TEXT) LIKE %s" for column in columnsToSearch
             ]
             whereClause = " OR ".join(conditions)
-            table = "stocklevels" if tabs.selected_index == 2 else "historicalsales"
+            table = (
+                "stocklevels"
+                if tableSearchSelection.value == "Stock Levels"
+                else "historicalsales"
+            )
             sqlQuery = f"SELECT * FROM {table} WHERE {whereClause}"
             params = [f"%{query}%"] * len(columnsToSearch)
             cursor.execute(sqlQuery, params)
@@ -97,15 +120,15 @@ def main(page: ft.Page):
                 rows.append(
                     ft.DataRow(cells=[ft.DataCell(ft.Text(str(cell))) for cell in row])
                 )
-            if tabs.selected_index == 2:
+            if tableSearchSelection.value == "Stock Levels":
                 searchStockLevelsTable.rows = rows
-            elif tabs.selected_index == 3:
+            elif tableSearchSelection.value == "Historical Sales":
                 searchHistoricalSalesTable.rows = rows
             page.update()
         else:
-            if tabs.selected_index == 2:
+            if tableSearchSelection.value == "Stock Levels":
                 searchStockLevelsTable.rows = []
-            elif tabs.selected_index == 3:
+            elif tableSearchSelection.value == "Historical Sales":
                 searchHistoricalSalesTable.rows = []
             searchHistoricalSalesTable.visible = False
             searchStockLevelsTable.visible = False
@@ -146,6 +169,34 @@ def main(page: ft.Page):
         on_click=lambda _: page.window_close(),
     )
 
+    tableBrowseSelection = ft.Dropdown(
+        label="Select Table to Browse",
+        label_style=ft.TextStyle(color="#e1e3e3", weight=ft.FontWeight.BOLD),
+        options=[
+            ft.dropdown.Option("Stock Levels"),
+            ft.dropdown.Option("Historical Sales"),
+        ],
+        on_change=tableSwitcher,
+        expand=True,
+        border_radius=10,
+        border_color=ft.colors.TRANSPARENT,
+        visible=True,
+    )
+
+    tableSearchSelection = ft.Dropdown(
+        label="Select Table to Search",
+        label_style=ft.TextStyle(color="#e1e3e3", weight=ft.FontWeight.BOLD),
+        options=[
+            ft.dropdown.Option("Stock Levels"),
+            ft.dropdown.Option("Historical Sales"),
+        ],
+        visible=False,
+        on_change=search,
+        expand=True,
+        border_radius=10,
+        border_color=ft.colors.TRANSPARENT,
+    )
+
     searchBar = ft.TextField(
         label="Press Enter to Search",
         expand=True,
@@ -172,6 +223,7 @@ def main(page: ft.Page):
         horizontal_lines=ft.border.BorderSide(0, "#004f58"),
         heading_text_style=ft.TextStyle(color="#e1e3e3", weight=ft.FontWeight.BOLD),
         data_text_style=ft.TextStyle(color="#e1e3e3"),
+        visible=False,
     )
 
     historicalSalesTable = ft.DataTable(
@@ -182,6 +234,7 @@ def main(page: ft.Page):
         horizontal_lines=ft.border.BorderSide(0, "#004f58"),
         heading_text_style=ft.TextStyle(color="#e1e3e3", weight=ft.FontWeight.BOLD),
         data_text_style=ft.TextStyle(color="#e1e3e3"),
+        visible=False,
     )
 
     addDataToTable(stockLevelsTable, fetchStockLevels, 10, stockLevelsTable.rows)
@@ -234,39 +287,39 @@ def main(page: ft.Page):
         indicator_border_radius=10,
         tabs=[
             ft.Tab(
-                text="Browse Stock Levels",
+                text="Browse",
                 icon=ft.icons.TABLE_ROWS,
                 content=ft.Column(
-                    [stockLevelsTable], scroll=True, expand=True, on_scroll=onScroll
+                    [stockLevelsTable, historicalSalesTable],
+                    scroll=True,
+                    expand=True,
+                    on_scroll=onScroll,
                 ),
             ),
             ft.Tab(
-                text="Browse Hisorical Sales",
-                icon=ft.icons.TABLE_CHART,
-                content=ft.Column(
-                    [historicalSalesTable], scroll=True, expand=True, on_scroll=onScroll
-                ),
-            ),
-            ft.Tab(
-                text="Search Stock Levels",
+                text="Search",
                 icon=ft.icons.SEARCH,
                 content=ft.Container(
-                    ft.Column([searchStockLevelsTable], scroll=True, expand=True),
+                    ft.Column(
+                        [searchStockLevelsTable, searchHistoricalSalesTable],
+                        scroll=True,
+                        expand=True,
+                    ),
                 ),
             ),
-            ft.Tab(
-                text="Search Historical Sales",
-                icon=ft.icons.SEARCH,
-                content=ft.Column(
-                    [searchHistoricalSalesTable], scroll=True, expand=True
-                ),
-            ),
+            ft.Tab(text="Charts", icon=ft.icons.AUTO_GRAPH),
         ],
         expand=True,
         on_change=search,
     )
 
-    bar = ft.Column([ft.Row([windowDragArea, btnClose]), ft.Row([searchBar])])
+    bar = ft.Column(
+        [
+            ft.Row([windowDragArea, btnClose]),
+            ft.Row([tableBrowseSelection]),
+            ft.Row([searchBar, tableSearchSelection]),
+        ]
+    )
 
     titleBar = ft.Row(
         [
